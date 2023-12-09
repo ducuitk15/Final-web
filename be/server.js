68c4db782f84bddb075479165b2a565e2c93765e
ducuitk15 <20521199@gm.uit.edu.vn>
2023-12-09 20:38:47 +0700

    First commit

---
+++ be/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+
+
+const app = express();
+dotenv.config().parsed;
+
+connectDB();
+
+app.get('/', (req,res) => {
+    res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Start on port ${PORT}`));
